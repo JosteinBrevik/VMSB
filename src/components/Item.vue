56c160d4240e08b37b82d3b3f3bb09b6f08b1d65
Jostein <jostein.brevik@gmail.com>
2019-02-15 14:08:30 +0100

    Add item component

---
+++ src/components/Item.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item">DETTE ER EN ITEM</div>
+</template>
+
+<script>
+export default {
+  name: "itemBox",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.item {
+  border-color: cyan;
+  margin: 10px;
+  padding: 20px;
+  background-color: lightgrey;
+  width: 200px;
+  align-self: center;
+}
+
+.item:hover {
+  background-color: grey;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+//import App from './App.vue';
+import App from './pages/Main.vue';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
